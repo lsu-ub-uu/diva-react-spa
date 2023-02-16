f68bd99909fc922b280c60750c649f936a709734
erikl <erilundin@gmail.com>
2023-02-16 22:50:48 +0100

    Added mui with emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,94 @@
-import reactLogo from './assets/react.svg';
 import './App.css';
-import { Button } from './components';
+import {
+  AppBar,
+  Box,
+  Breadcrumbs,
+  Button,
+  Container,
+  Link,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
 function App() {
   return (
-    <div className='App'>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src='/vite.svg'
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
-        <a
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Button label='test' />
-      <div className='card'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
+    <>
+      <AppBar
+        position='fixed'
+        sx={{ backgroundColor: '#75598E' }}
+      >
+        <Toolbar>
+          <Typography
+            variant='h6'
+            color='inherit'
+            noWrap
+          >
+            DiVA
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth='lg'>
+        <Box sx={{ my: 4 }}>
+          <Breadcrumbs aria-label='breadcrumb'>
+            <Link
+              underline='hover'
+              color='inherit'
+              href='/'
+            >
+              Start
+            </Link>
+            <Link
+              underline='hover'
+              color='inherit'
+              href='/'
+            >
+              Page 2
+            </Link>
+            <Typography color='text.primary'>Breadcrumbs</Typography>
+          </Breadcrumbs>
+          <Typography
+            variant='h5'
+            component='h1'
+            gutterBottom
+          >
+            Digitala Vetenskapliga Arkivet
+          </Typography>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs
+              disableRipple
+              value={0}
+              aria-label='basic tabs example'
+            >
+              <Tab label='Item One' />
+              <Tab label='Item Two' />
+              <Tab label='Item Three' />
+            </Tabs>
+          </Box>
+          <Typography
+            sx={{ mt: 6, mb: 3 }}
+            color='text.secondary'
+          >
+            Some secondary text
+          </Typography>
+          <Button
+            disableRipple
+            variant='contained'
+            color='primary'
+          >
+            Primary
+          </Button>
+          <Button
+            disableRipple
+            variant='outlined'
+            color='secondary'
+          >
+            Secondary
+          </Button>
+        </Box>
+      </Container>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { theme } from './theme';
 import App from './App';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
