1410c4b383be6f87f4cc4eb79db48b5787019a0a
Erik Lundin <erilundin@gmail.com>
2023-03-02 09:54:23 +0100

    Added automatic language detector

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,8 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources: {
       en: {
@@ -20,7 +22,6 @@ i18n
         },
       },
     },
-    lng: 'sv',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
