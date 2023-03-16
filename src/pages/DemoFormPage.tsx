f3b19e546c47d12b06e14c92c4123a03d06660b7
erikl <erilundin@gmail.com>
2023-03-16 22:36:36 +0100

    Added dummy search facets

--- src/pages/DemoFormPage.tsx
+++ src/pages/DemoFormPage.tsx
@@ -1,10 +1,53 @@
-import { FormControl, FormLabel } from '@mui/material';
-import React from 'react';
-import { Card, Search } from '../components';
+import {
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Stack,
+  Typography,
+} from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import { AsidePortal, Card, Checkbox, Search } from '../components';
 
 export const DemoFormPage = () => {
+  const methods = useForm();
+  const { control } = methods;
   return (
     <div>
+      <AsidePortal>
+        <Stack spacing={1}>
+          <Typography sx={{ fontWeight: 'bold' }}>Publication types</Typography>
+          <Controller
+            control={control}
+            name='facet-demo-1'
+            defaultValue=''
+            render={({ field }) => (
+              <FormGroup {...field}>
+                <FormControlLabel
+                  value='choice-1'
+                  control={<Checkbox />}
+                  label='Articles (4500)'
+                />
+                <FormControlLabel
+                  value='choice-2'
+                  control={<Checkbox />}
+                  label='Bok (234)'
+                />
+                <FormControlLabel
+                  value='choice-3'
+                  control={<Checkbox />}
+                  label='Tidskrifter (34)'
+                />
+                <FormControlLabel
+                  value='choice-4'
+                  control={<Checkbox />}
+                  label='Patent (10)'
+                />
+              </FormGroup>
+            )}
+          />
+        </Stack>
+      </AsidePortal>
       <Card
         title='Search'
         variant='variant1'
