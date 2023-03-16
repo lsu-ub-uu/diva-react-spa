ad2762f23276c3eb2b419fa4fefaa927bfddf899
Erik Lundin <erilundin@gmail.com>
2023-03-16 14:49:12 +0100

    Removed console debug

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter } from 'react-router-dom';
 import './app/i18n';
 import Router from './routes/routes';
 
-console.log('aaaa', import.meta.env.BASE_URL);
-
 const App = () => {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
