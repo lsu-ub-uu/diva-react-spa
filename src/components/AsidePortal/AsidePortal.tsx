826e471aa1b375262c7bed4927d314399b0e2ec0
egiiiiil <sixteenth.bird@gmail.com>
2023-03-27 17:38:59 +0200

    Added sticky behavior to AsidePortal on scroll

--- src/components/AsidePortal/AsidePortal.tsx
+++ src/components/AsidePortal/AsidePortal.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
@@ -5,17 +6,51 @@ interface AsidePortalProps {
   element?: Element;
   children: React.ReactNode;
 }
+interface StyledAsidePortalProps {
+  fixed?: boolean;
+}
+
+const StyledAsidePortal = styled('div')((props: StyledAsidePortalProps) => ({
+  position: props.fixed === true ? 'fixed' : 'relative',
+  transition: 'position 0.5s ease',
+  top: props.fixed === true ? '50px' : '0',
+}));
 
 export const AsidePortal = (props: AsidePortalProps) => {
+  const [fixed, setFixed] = useState(false);
+
+  const setFixedSidebar = () => {
+    if (window.scrollY >= 140) {
+      setFixed(true);
+    } else {
+      setFixed(false);
+    }
+  };
+  window.addEventListener('scroll', setFixedSidebar);
+  useEffect(() => {
+    console.log(fixed);
+  }, [fixed]);
+
   const [element, setElement] = useState<Element | null>(null);
 
   useEffect(() => {
     const attachElement = props.element
       ? props.element
       : document.querySelector('#sidebar-content');
-
     setElement(attachElement);
   }, [props.element]);
 
-  return element ? createPortal(props.children, element) : <p>Error portal</p>;
+  return element ? (
+    createPortal(
+      <StyledAsidePortal
+        fixed={fixed}
+        className='aaaa'
+      >
+        {props.children}
+      </StyledAsidePortal>,
+      element,
+    )
+  ) : (
+    <p>Error portal</p>
+  );
 };
