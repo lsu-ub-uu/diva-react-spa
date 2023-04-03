60e1c807c29b6235e89bd0ec85ad8140be903dbb
egiiiiil <sixteenth.bird@gmail.com>
2023-04-03 17:16:22 +0200

    Added styling for snackbars

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, styled } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import { Provider as StateProvider } from 'react-redux';
 import store from './app/store';
@@ -14,15 +14,46 @@ if (process.env.NODE_ENV === 'development') {
   worker.start().then();
 }
 
+const StyledSnackbarProvider = styled(SnackbarProvider)(() => ({
+  '&.notistack-MuiContent': {
+    backgroundColor: '#efefef',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  '&.notistack-MuiContent-success': {
+    svg: {
+      color: 'rgb(56, 142, 60)',
+      fill: 'rgb(56, 142, 60)',
+    },
+  },
+  '&.notistack-MuiContent-error': {
+    svg: {
+      color: 'rgb(211, 47, 47)',
+      fill: 'rgb(211, 47, 47)',
+    },
+  },
+  '&.notistack-MuiContent-warning': {
+    svg: {
+      color: 'rgb(245, 124, 0)',
+      fill: 'rgb(245, 124, 0)',
+    },
+  },
+  '&.notistack-MuiContent-info': {
+    svg: {
+      color: 'rgb(2, 136, 209)',
+      fill: 'rgb(2, 136, 209)',
+    },
+  },
+}));
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BackdropProvider>
       <StateProvider store={store}>
         <ThemeProvider theme={divaTheme}>
           <CssBaseline />
-          <SnackbarProvider maxSnack={5}>
+          <StyledSnackbarProvider maxSnack={5}>
             <App />
-          </SnackbarProvider>
+          </StyledSnackbarProvider>
         </ThemeProvider>
       </StateProvider>
     </BackdropProvider>
