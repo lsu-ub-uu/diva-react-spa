278f637389b0d5ddea96c5b3cd846cabe2907e4a
egiiiiil <sixteenth.bird@gmail.com>
2023-04-13 11:15:53 +0200

    Added expect, removed debug

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -29,6 +29,6 @@ describe('<App />', () => {
       </BackdropProvider>,
     );
     const logoImage = screen.getByAltText('logo');
-    screen.debug(logoImage);
+    expect(logoImage).toBeInTheDocument();
   });
 });
