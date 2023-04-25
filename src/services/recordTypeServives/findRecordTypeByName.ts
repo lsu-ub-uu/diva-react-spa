19cfee3b3ea2e7e88df938b9e5fe45a9c756e1d3
egiiiiil <sixteenth.bird@gmail.com>
2023-04-25 10:59:22 +0200

    Fix to saving id of recordType

--- src/services/recordTypeServives/findRecordTypeByName.ts
+++ src/services/recordTypeServives/findRecordTypeByName.ts
@@ -1,8 +1,6 @@
 export const findRecordTypeByName = (responseArray: any) => {
-  const tsUpdatedArray: any[] = [];
   const returnObject = {
     id: '',
-    tsUpdated: tsUpdatedArray,
   };
   responseArray.forEach((data: any) => {
     return Object.entries(data).forEach((children) => {
@@ -12,30 +10,8 @@ export const findRecordTypeByName = (responseArray: any) => {
             c.forEach((d: string) => {
               if (Object.values(d).includes('id')) {
                 // Adding to returnObject
-                ({ id: Object.values(d)[1] } = returnObject);
-              }
-              if (Object.keys(d).includes('children')) {
-                Object.entries(d).forEach((e) => {
-                  if (e.includes('children')) {
-                    Object.entries(e).forEach((f) => {
-                      if (f.includes('1')) {
-                        f.forEach((g) => {
-                          if (Array.isArray(g)) {
-                            g.forEach((name) => {
-                              if (Object.values(name).includes('tsUpdated')) {
-                                Object.entries(name).forEach((value) => {
-                                  if (value.includes('value')) {
-                                    tsUpdatedArray.push(value[1]);
-                                  }
-                                });
-                              }
-                            });
-                          }
-                        });
-                      }
-                    });
-                  }
-                });
+                returnObject.id = Object.values(d)[1];
+                /* ({ id: Object.values(d)[1] } = returnObject); */
               }
             });
           }
