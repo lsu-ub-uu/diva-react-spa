47482bcb03327cb6fbd9ca19d097f51eec2d98c6
egiiiiil <sixteenth.bird@gmail.com>
2023-05-12 15:51:09 +0200

    Added test for radio button

--- src/components/Form/Radio/__tests__/Radio.test.tsx
+++ src/components/Form/Radio/__tests__/Radio.test.tsx
@@ -2,6 +2,7 @@ import { test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { FormControlLabel } from '@mui/material';
 import { Radio } from '../Radio';
+import userEvent from '@testing-library/user-event';
 
 /**
  * @vitest-environment jsdom
@@ -20,5 +21,23 @@ describe('<Radio />', () => {
     );
     const button = screen.getByLabelText('Female');
     expect(button).toBeInTheDocument();
+  });
+  test('Option gets selected on click', async () => {
+    render(
+      <form>
+        <FormControlLabel
+          value='female'
+          control={<Radio />}
+          label='Female'
+        />
+      </form>,
+    );
+    const user = userEvent.setup();
+    const radio = screen.getByRole('radio', { name: 'Female' });
+
+    expect(radio).not.toBeChecked();
+
+    await user.click(radio);
+    expect(radio).toBeChecked();
   });
 });
