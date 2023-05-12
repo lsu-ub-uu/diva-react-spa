5c970ec59bc7d0daae3e235aeea5afe2b0400c66
egiiiiil <sixteenth.bird@gmail.com>
2023-05-12 16:48:51 +0200

    Work on fetching mock data in test

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -4,7 +4,7 @@ import researchSubjects from './data/researchTestTree.json';
 import fakePersons from './data/fakePersons.json';
 import subjectCategories from './data/subjectCategories.json';
 
-const URL = `${window.location.protocol}//${window.location.host}`;
+// const URL = `${window.location.protocol}//${window.location.host}`;
 const BFF_API_URL = import.meta.env.VITE_BFF_API_URL;
 
 export const handlers = [
@@ -20,16 +20,55 @@ export const handlers = [
   rest.get(`${URL}/subject-categories`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(subjectCategories));
   }),
+  rest.get(`${BFF_API_URL}/search/egil`, (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          givenName: 'Egil',
+          familyName: 'Andersson',
+          domain: 'uu',
+          ORCID_ID: '',
+          academicTitle: '',
+          id: 'authority-person:14094',
+        },
+        {
+          givenName: 'Egil',
+          familyName: 'Henriksen',
+          domain: 'uu',
+          ORCID_ID: '',
+          academicTitle: '',
+          id: 'authority-person:13750',
+        },
+        {
+          givenName: 'Egil',
+          familyName: 'Asprem',
+          domain: 'su',
+          ORCID_ID: '0000-0001-9944-1241',
+          academicTitle: 'Professor',
+          id: 'authority-person:84704',
+        },
+        {
+          givenName: 'Daniel Egil',
+          familyName: 'Yencken',
+          domain: 'su',
+          ORCID_ID: '0000-0002-5282-6424',
+          academicTitle: '',
+          id: 'authority-person:95060',
+        },
+      ]),
+    );
+  }),
   rest.post('/upload-file', (_req, res, ctx) => {
     return res(ctx.status(200));
   }),
-  rest.get(`${URL}/*`, (req) => {
-    return req.passthrough();
-  }),
-  rest.get(`${BFF_API_URL}/*`, (req) => {
-    return req.passthrough();
-  }),
-  rest.get(`http://localhost:8081/api/*`, (req) => {
-    return req.passthrough();
-  }),
+  // rest.get(`${URL}/*`, (req) => {
+  //   return req.passthrough();
+  // }),
+  // rest.get(`${BFF_API_URL}/*`, (req) => {
+  //   return req.passthrough();
+  // }),
+  // rest.get(`http://localhost:8081/api/*`, (req) => {
+  //   return req.passthrough();
+  // }),
 ];
---
+++ src/features/search/__tests__/searchPersonsAsAdmin.test.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { rest } from 'msw';
+import { server } from '../../../__mocks__/server';
+import searchService from '../searchService';
+
+describe('searchPersonsAsAdmin', () => {
+  test('Returning search data', async () => {
+    const response = await axios.get(
+      `https://cora.epc.ub.uu.se/diva/spaclientbff/api/search/admin/person/egil`,
+    );
+    console.log(response);
+    // const search = await searchService.searchPersonsAsAdmin('egil');
+    // console.log(search);
+  });
+});
