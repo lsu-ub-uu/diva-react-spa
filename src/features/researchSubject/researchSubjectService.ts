fc28213f1b38af7cabe4960008d7c2a03ecc1e9c
erikl <erilundin@gmail.com>
2023-05-15 13:17:51 +0200

    Removed comment

--- src/features/researchSubject/researchSubjectService.ts
+++ src/features/researchSubject/researchSubjectService.ts
@@ -32,7 +32,6 @@ export const loadResearchSubjectsAsync = async () => {
   const response = await fetch(`${MOCK_API_URL}/research-subjects`);
   const data: RenderTree = await response.json();
   return getFlat(data, '-').sort((a, b) => a.name.localeCompare(b.name, 'sv'));
-  // const treeData = getTree(flatData, '-');
 };
 
 const researchSubjectService = {
