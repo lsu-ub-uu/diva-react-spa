79c44b935372697e6ae22709fa0893b073b03566
Erik Lundin <erilundin@gmail.com>
2023-05-16 09:32:42 +0200

    Added general utils for hierarchical transforms

--- src/features/subjectCategory/subjectCategoryService.ts
+++ src/features/subjectCategory/subjectCategoryService.ts
@@ -1,21 +1,12 @@
 import { RenderTree } from 'components/RichTree/RichTree';
-import { SelectItem } from 'components';
+import { getFlat } from '../../utils';
 
 const MOCK_API_URL = `${window.location.protocol}//${window.location.host}`;
-
-function getFlat(node: RenderTree): SelectItem[] {
-  return [
-    { id: node.id, name: node.name, disabled: node.disabled } as SelectItem,
-  ].concat(...(node.children?.map(getFlat) ?? []));
-}
 
 const loadSubjectCategoriesAsync = async () => {
   const response = await fetch(`${MOCK_API_URL}/subject-categories`);
   const data: RenderTree = await response.json();
-  const flatten = getFlat(data).sort((a, b) =>
-    a.name.localeCompare(b.name, 'sv'),
-  );
-  return flatten;
+  return getFlat(data, '-').sort((a, b) => a.name.localeCompare(b.name, 'sv'));
 };
 
 const subjectCategoryService = {
