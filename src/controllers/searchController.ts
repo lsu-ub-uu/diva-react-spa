e27d37e7987df2e03a3654d15bc4849cc2a61694
Jerkovic <erilundin@gmail.com>
2023-09-15 09:15:04 +0200

    Fixed search controller trying to call service not existing

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,9 +1,4 @@
 import { Request, Response } from 'express';
-// import { searchPersonsByGeneralSearch } from '../services/searchServives/api/api';
-import { searchPersonsByGeneralSearch } from '../services/searchServices/searchPersonByGeneralSearch';
-
-// const { PROD_CORA_API_URL } = process.env;
-const PROD_CORA_API_URL = 'https://cora.diva-portal.org/diva/rest/';
 
 // @desc		Get admin search results
 // @route		GET /api/search/test/person/:searchQuery
@@ -14,14 +9,10 @@ export const getSearchPersonsByGeneralSearch = async (
   error: unknown,
 ) => {
   const { searchQuery } = req.params;
-  const { start, rows } = req.query;
+  // const { start, rows } = req.query;
 
   try {
-    const searchParam = await searchPersonsByGeneralSearch(
-      `${searchQuery}`,
-      Number(start as string),
-      Number(rows as string),
-    );
+    const searchParam: string[] = [];
     res.status(200).json(searchParam);
   } catch {
     res.status(404).json({ error: `No results for ${searchQuery} found` });
