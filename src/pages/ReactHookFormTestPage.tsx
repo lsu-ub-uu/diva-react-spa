7e28e217f93f8409dd71044d9786a745cbda5af6
Erik Lundin <erilundin@gmail.com>
2023-09-26 08:09:39 +0200

    Cleanup repeating form field spike test

--- src/pages/ReactHookFormTestPage.tsx
+++ src/pages/ReactHookFormTestPage.tsx
@@ -1,15 +1,5 @@
-import {
-  Box,
-  ButtonGroup,
-  FormControl,
-  FormLabel,
-  Grid,
-  IconButton,
-  Stack,
-} from '@mui/material';
+import { Box, ButtonGroup, Grid, IconButton, Stack } from '@mui/material';
 import Button from '@mui/material/Button';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { useFieldArray, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -17,24 +7,13 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-import { Card, ErrorSummary, Option } from '../components';
-import {
-  ControlledTextField,
-  ControlledMultiCheckboxField,
-  ControlledRadioButtons,
-  ControlledDateTimePicker,
-  ControlledEditor,
-} from '../components/Controlled';
+
+import { Card, ErrorSummary } from '../components';
+import { ControlledTextField } from '../components/Controlled';
 
 const validationSchema = yup.object().shape({
-  title: yup.string().trim().required(),
-  startDateTime: yup.date().typeError('Invalid Date!'),
-  radioValue: yup.string().trim().required(),
-  checkboxValues: yup.array().of(yup.string()).min(1).max(2),
   // eslint-disable-next-line react/forbid-prop-types
-  someTextVariables: yup
+  someTextVariable: yup
     .array()
     .of(
       yup.object().shape({
@@ -49,18 +28,16 @@ const validationSchema = yup.object().shape({
 });
 
 export const ReactHookFormTestPage = () => {
-  const { t } = useTranslation();
   const methods = useForm({
     shouldFocusError: false,
     resolver: yupResolver(validationSchema),
     defaultValues: {
-      title: '<p>this is the title</p>',
-      startDateTime: '2001-09-01 16:30',
-      radioValue: '',
-      checkboxValues: [],
-      someTextVariables: [
+      someTextVariable: [
         {
-          someTextVariable: '',
+          someTextVariable: '1',
+        },
+        {
+          someTextVariable: '2',
         },
       ],
     },
@@ -71,7 +48,7 @@ export const ReactHookFormTestPage = () => {
     formState: { errors },
   } = methods;
   const { fields, append, remove, move } = useFieldArray({
-    name: 'someTextVariables',
+    name: 'someTextVariable',
     control,
   });
 
@@ -87,137 +64,38 @@ export const ReactHookFormTestPage = () => {
     console.log(data);
   };
 
-  const optionsTest: Option[] = [
-    {
-      label: 'Option 1',
-      value: '1',
-      disabled: true,
-    },
-    {
-      label: 'Option 2',
-      value: '2',
-    },
-    {
-      label: 'Option 3',
-      value: '3',
-    },
-    {
-      label: 'Option 4',
-      value: '4',
-    },
-  ];
-
-  const getMaxRepeatingFromValidation = () => {
-    const schemaFieldDescription =
-      validationSchema.fields.someTextVariables.describe();
+  const getFieldValidationPropertyValue = (
+    fieldName: string,
+    ruleName: string,
+  ): number => {
     // @ts-ignore
-    return schemaFieldDescription.tests[1].params.max;
-  };
+    const fieldRules = validationSchema.fields[fieldName]?.describe();
 
-  const getMinRepeatingFromValidation = () => {
-    const schemaFieldDescription =
-      validationSchema.fields.someTextVariables.describe();
-    // @ts-ignore
-    return schemaFieldDescription.tests[0].params.min;
+    if (!fieldRules) {
+      console.error(`Validation rules for field "${fieldName}" not found.`);
+      return 1;
+    }
+
+    const rule = fieldRules.tests.find((r: any) => r.name === ruleName);
+    return rule ? rule.params[ruleName] : 1;
   };
 
   // @ts-ignore
   return (
-    <>
-      <Helmet>
-        <title>{t('addpub')} | DiVA</title>
-      </Helmet>
-
-      <Box
-        component='form'
-        onSubmit={handleSubmit(handleOnSubmit)}
-      >
-        <Stack spacing={1}>
-          <div>
-            <ErrorSummary errors={errors} />
-          </div>
-          <Card
-            title='Testing'
-            variant='variant6'
-            tooltipTitle='Title'
-            tooltipBody='Here goes some text about how choose type'
-          >
-            <Grid
-              container
-              spacing={2}
-              justifyContent='space-between'
-              alignItems='flex-start'
-            >
-              <Grid
-                item
-                xs={12}
-              >
-                <ControlledEditor
-                  required
-                  toolbar='italic | alignleft aligncenter alignright | fullscreen | code paste charmap superscript subscript'
-                  control={control}
-                  name='title'
-                  label='Title'
-                  plugins='code fullscreen paste charmap'
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={8}
-              >
-                <ControlledMultiCheckboxField
-                  required
-                  options={optionsTest}
-                  control={control}
-                  name='checkboxValues'
-                  label='Checkbox Input'
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={8}
-              >
-                <ControlledRadioButtons
-                  options={optionsTest}
-                  control={control}
-                  name='radioValue'
-                  label='Radios label'
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={6}
-              >
-                <ControlledDateTimePicker
-                  control={control}
-                  name='startDateTime'
-                  label='Start datetime'
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sm={4}
-              >
-                <FormControl
-                  fullWidth
-                  sx={{ mb: 2 }}
-                >
-                  <FormLabel>&nbsp;</FormLabel>
-                  <Button
-                    size='medium'
-                    disableRipple
-                    variant='outlined'
-                  >
-                    Change Publication Type
-                  </Button>
-                </FormControl>
-              </Grid>
-            </Grid>
-          </Card>
+    <Box
+      component='form'
+      onSubmit={handleSubmit(handleOnSubmit)}
+    >
+      <Stack spacing={1}>
+        <div>
+          <ErrorSummary errors={errors} />
+        </div>
+        <Card
+          title='Testing'
+          variant='variant6'
+          tooltipTitle='Title'
+          tooltipBody='Here goes some text about how choose type'
+        >
           {fields.map((item, index) => {
             return (
               <Grid
@@ -235,7 +113,7 @@ export const ReactHookFormTestPage = () => {
                   <ControlledTextField
                     placeholder='some placeholder'
                     control={control}
-                    name={`someTextVariables.${index}.someTextVariable`}
+                    name={`someTextVariable.${index}.someTextVariable`}
                     label='Somelabel'
                   />
                 </Grid>
@@ -264,7 +142,11 @@ export const ReactHookFormTestPage = () => {
                     <IconButton
                       aria-label='delete'
                       disabled={
-                        fields.length === getMinRepeatingFromValidation()
+                        fields.length <=
+                        getFieldValidationPropertyValue(
+                          'someTextVariable',
+                          'min',
+                        )
                       }
                       onClick={() => handleRemove(index)}
                     >
@@ -277,49 +159,40 @@ export const ReactHookFormTestPage = () => {
           })}
 
           <Button
-            disabled={fields.length >= getMaxRepeatingFromValidation()}
+            fullWidth
+            variant='outlined'
+            disabled={
+              fields.length >=
+              getFieldValidationPropertyValue('someTextVariable', 'max')
+            }
             disableRipple
             onClick={() => append({ someTextVariable: '' })}
             endIcon={<AddCircleOutlineIcon />}
           >
             Add
           </Button>
+        </Card>
+        <Grid
+          container
+          direction='row'
+          justifyContent='space-between'
+          alignItems='flex-start'
+        >
           <Grid
-            container
-            direction='row'
-            justifyContent='space-between'
-            alignItems='flex-start'
+            item
+            xs={12}
           >
-            <Grid
-              item
-              xs={6}
+            <Button
+              fullWidth
+              type='submit'
+              disableRipple
+              variant='contained'
             >
-              <Button
-                disabled
-                disableRipple
-                variant='outlined'
-                startIcon={<ArrowBackIcon />}
-              >
-                Back
-              </Button>
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              sx={{ textAlign: 'right' }}
-            >
-              <Button
-                type='submit'
-                disableRipple
-                variant='contained'
-                endIcon={<ArrowForwardIcon />}
-              >
-                Continue
-              </Button>
-            </Grid>
+              Submit
+            </Button>
           </Grid>
-        </Stack>
-      </Box>
-    </>
+        </Grid>
+      </Stack>
+    </Box>
   );
 };
