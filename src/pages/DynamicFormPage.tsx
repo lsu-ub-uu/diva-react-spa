32b9d2d5a20694467efdb513dc2939b4716734fb
Jerkovic <erilundin@gmail.com>
2023-11-15 16:19:05 +0100

    Added first draft of the form shortcut sidebar

--- src/pages/DynamicFormPage.tsx
+++ src/pages/DynamicFormPage.tsx
@@ -20,11 +20,18 @@
 import { useTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet-async';
 import React, { useEffect, useState } from 'react';
-import { Alert, Skeleton, Stack } from '@mui/material';
+import {
+  Alert,
+  Skeleton,
+  Stack,
+  Step,
+  StepButton,
+  Stepper,
+} from '@mui/material';
 import axios from 'axios';
 import { useSnackbar, VariantType } from 'notistack';
 import { FieldValues } from 'react-hook-form';
-import { useBackdrop, FormGenerator } from '../components';
+import { useBackdrop, FormGenerator, AsidePortal } from '../components';
 import { useCoraFormSchemaByValidationType } from '../app/hooks';
 import { FormSchema } from '../components/FormGenerator/types';
 import {
@@ -81,6 +88,52 @@ export const DynamicFormPage = () => {
 
   return (
     <>
+      <AsidePortal>
+        <Stepper
+          sx={{
+            '& .MuiStepper-root': {
+              width: 'inherit',
+              height: 'inherit',
+            },
+            '& .MuiSvgIcon-root': {
+              borderRadius: '50%',
+              stroke: '#613985',
+              strokeWidth: '3px',
+            },
+            '& .MuiSvgIcon-root:not(.Mui-completed)': {
+              color: 'white',
+            },
+            '& .MuiStepIcon-text': {
+              fill: '#613985',
+              display: 'none',
+            },
+            '& .MuiSvgIcon-root.Mui-active': {
+              color: '#613985',
+              stroke: '#613985',
+              strokeWidth: '3px',
+            },
+            '& .MuiSvgIcon-root.Mui-completed': {
+              color: '#c1b3ce',
+              stroke: '#c1b3ce',
+              strokeWidth: '3px',
+            },
+          }}
+          orientation='vertical'
+          nonLinear
+          activeStep={5}
+        >
+          {schema?.form.components?.map((item) => (
+            <Step key={item.name}>
+              <StepButton
+                disableRipple
+                href='#'
+              >
+                {t(item.label as string) as string}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+      </AsidePortal>
       <Helmet>
         <title>{t('Name of form')} | DiVA</title>
       </Helmet>
