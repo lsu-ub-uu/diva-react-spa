e3d870806641f2b4a98d83495221157158eee2b3
Jerkovic <erilundin@gmail.com>
2023-11-20 12:10:15 +0100

    CORA-2361 Added first draft of parsing cora record

---
+++ src/config/__tests__/transformRecord.test.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with DiVA Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import recordManuscript from '../../__mocks__/coraRecordManuscript.json';
+import { transformRecord } from '../transformRecord';
+import { RecordWrapper } from '../../utils/cora-data/CoraData';
+
+describe('transformRecord', () => {
+  it('should return a record id', () => {
+    const transformData = transformRecord(recordManuscript as RecordWrapper);
+    const expected =
+    {
+      id: 'divaOutput:519333261463755',
+      createdAt: '2023-10-11T09:24:30.511487Z',
+      createdBy: 'coraUser:490742519075086',
+      updated: [
+        {
+          updateAt: '2023-10-11T09:24:30.511487Z',
+          updatedBy: 'coraUser:490742519075086'
+        },
+        {
+          updateAt: '2023-10-18T09:09:13.554736Z',
+          updatedBy: '161616'
+        },
+        {
+          updateAt: '2023-10-26T12:33:22.260532Z',
+          updatedBy: '161616'
+        },
+        {
+          updateAt: '2023-10-26T12:35:28.748398Z',
+          updatedBy: '161616'
+        },
+        {
+          updateAt: '2023-10-26T12:35:40.545698Z',
+          updatedBy: '161616'
+        },
+        {
+          updateAt: '2023-10-26T12:35:52.293623Z',
+          updatedBy: '161616'
+        }
+      ]
+
+    }
+    expect(transformData).toStrictEqual(expected);
+  });
+});
---
+++ src/config/transformRecord.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with DiVA Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { DataGroup, RecordWrapper } from '../utils/cora-data/CoraData';
+import { extractIdFromRecordInfo } from '../utils/cora-data/CoraDataTransforms';
+import {
+  getAllChildrenWithNameInData,
+  getFirstDataGroupWithNameInData,
+} from '../utils/cora-data/CoraDataUtils';
+import { getFirstDataAtomicValueWithNameInData } from '../utils/cora-data/CoraDataUtilsWrappers';
+import { extractLinkedRecordIdFromNamedRecordLink } from './transformValidationTypes';
+
+
+const extractRecordInfoDataGroup = (coraRecordGroup: DataGroup): DataGroup => {
+  return getFirstDataGroupWithNameInData(coraRecordGroup, 'recordInfo') as DataGroup;
+};
+
+const extractRecordUpdates = (recordInfo: DataGroup): unknown[] => {
+  const updates = getAllChildrenWithNameInData(recordInfo, 'updated');
+  return updates.map((update) => {
+    const updatedGroup = update as DataGroup;
+    const updatedBy = extractLinkedRecordIdFromNamedRecordLink(updatedGroup, 'updatedBy');
+    const updateAt = getFirstDataAtomicValueWithNameInData(updatedGroup, 'tsUpdated');
+    return { updateAt, updatedBy };
+  });
+}
+
+export const transformRecord = (
+  recordWrapper: RecordWrapper,
+): unknown => {
+  const coraRecord = recordWrapper.record;
+  const dataRecordGroup = coraRecord.data;
+
+  const id = extractIdFromRecordInfo(dataRecordGroup);
+  const recordInfo = extractRecordInfoDataGroup(dataRecordGroup);
+  const createdAt = getFirstDataAtomicValueWithNameInData(recordInfo, 'tsCreated');
+  const createdBy = extractLinkedRecordIdFromNamedRecordLink(recordInfo, 'createdBy');
+
+  const updated = extractRecordUpdates(recordInfo);
+  return { id, createdAt, createdBy, updated };
+};
+
+
+
