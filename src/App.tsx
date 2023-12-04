ffafd80e6b094a9b66ef205f3541ced0d8e1ec1d
egiiiiil <sixteenth.bird@gmail.com>
2023-12-04 10:47:40 +0100

    Added dev favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { Helmet } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import './app/i18n';
 import Router from './routes/routes';
 
 const App = () => {
+  const location = window.location.href;
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <Router />
-    </BrowserRouter>
+    <>
+      <Helmet>
+        {location.startsWith('http://localhost') === true ? (
+          <link
+            rel='icon'
+            type='image/svg+xml'
+            href='/dev_favicon.svg'
+          />
+        ) : (
+          <link
+            rel='icon'
+            type='image/svg+xml'
+            href='/favicon.svg'
+          />
+        )}
+      </Helmet>
+
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Router />
+      </BrowserRouter>
+    </>
   );
 };
 
