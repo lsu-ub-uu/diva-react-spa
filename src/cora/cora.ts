b6b25bb3a76a6bc235d7539dfc0bbab8432f837f
Jerkovic <erilundin@gmail.com>
2023-12-06 17:15:00 +0100

    CORA-2384 Added first draft of search API to Cora

--- src/cora/cora.ts
+++ src/cora/cora.ts
@@ -33,6 +33,24 @@ export async function getRecordDataListByType<T>(
   return response;
 }
 
+export async function getSearchResultDataListBySearchType<T>(
+  searchType: string,
+  searchData: DataGroup,
+  authToken: string
+): Promise<AxiosResponse<T>> {
+  const apiUrl: string = `/record/searchResult/${searchType}`;
+
+  const queryString = ''; // todo handle searchData
+  const finalUrl = `${apiUrl}?${queryString}`;
+
+  const headers = {
+    Authtoken: `${authToken}`
+  };
+
+  const response: AxiosResponse<T> = await axios.get(finalUrl, { headers });
+  return response;
+}
+
 export async function getRecordDataById<T>(
   type: string,
   id: string,
