8fdc95552f479f847648134b525a84872c1e1d5c
egiiiiil <sixteenth.bird@gmail.com>
2024-01-08 12:01:46 +0100

    Removed MSW from dependencies

--- setupTest.js
+++ setupTest.js
@@ -1,15 +1,9 @@
 import { expect, afterEach, afterAll, beforeAll } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import matchers from '@testing-library/jest-dom/matchers';
-import { server } from './src/__mocks__/server';
 
 expect.extend(matchers);
 
-beforeAll(() => server.listen());
-
-// runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
-  server.resetHandlers();
   cleanup();
 });
-afterAll(() => server.close());
