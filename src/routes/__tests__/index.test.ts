81b38f0a5c2a374d2b6b5d287c0fe2527293b261
egiiiiil <sixteenth.bird@gmail.com>
2024-02-19 11:27:19 +0100

    Added test for routes

--- src/routes/__tests__/index.test.ts
+++ src/routes/__tests__/index.test.ts
@@ -38,6 +38,54 @@ describe('API endpoint routes', () => {
       // expect(response.body).toStrictEqual({ test: 'test' });
     }, 100000);
   });
+  describe('/api/record', () => {
+    it('GET /api/record/person/person:8016283357279641 returns a 200', async () => {
+      const refresh = await request(app).get('/api/refreshDefinitions');
+      expect(refresh.statusCode).toBe(200);
+      const response = await request(app).get('/api/record/person/person:8016283357279641');
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toStrictEqual({
+        id: 'person:8016283357279641',
+        recordType: 'person',
+        validationType: 'person',
+        createdAt: '2021-02-12T10:55:49.032650Z',
+        updated: [],
+        userRights: ['read', 'read_incoming_links'],
+        data: {
+          person: {
+            authorisedName: {
+              familyName: [{ value: 'Efternamn' }],
+              givenName: [{ value: 'Förnamn' }]
+            },
+            academicTitle: [{ value: 'Akademisk titel' }],
+            alternativeName: [
+              {
+                familyName: [{ value: 'Alternativt efternamn' }],
+                givenName: [{ value: 'Alternativt förnamn' }]
+              }
+            ],
+            externalURL: [{ linkTitle: { value: 'Länktext' }, URL: { value: 'http://xx.se' } }],
+            otherAffiliation: [
+              {
+                affiliation: { value: 'Annan organisation' },
+                affiliationFromYear: [{ value: '1980' }],
+                affiliationUntilYear: [{ value: '1990' }]
+              }
+            ],
+            ORCID_ID: [{ value: '1111-2222-3333-4444' }],
+            VIAF_ID: [{ value: '123456' }],
+            Libris_ID: [{ value: '123456' }],
+            biographyEnglish: [
+              { biography: { value: 'Biografi på engelska.' }, language: { value: 'en' } }
+            ],
+            biographySwedish: [
+              { biography: { value: 'Biografi på svenska.' }, language: { value: 'sv' } }
+            ]
+          }
+        }
+      });
+    }, 100000);
+  });
   describe('/api/refreshDefinitions', () => {
     it('GET /api/refreshDefinitions returns a 200', async () => {
       const response = await request(app).get('/api/refreshDefinitions');
