bb95fef37f3e0dd211d8bcf9931e5ad7d4392004
egiiiiil <sixteenth.bird@gmail.com>
2024-02-20 14:44:54 +0100

    CORA-2293 Updted test for api endpoint with presentation

--- src/routes/__tests__/index.test.ts
+++ src/routes/__tests__/index.test.ts
@@ -42,45 +42,222 @@ describe('API endpoint routes', () => {
     it('GET /api/record/person/person:8016283357279641 returns a 200', async () => {
       const refresh = await request(app).get('/api/refreshDefinitions');
       expect(refresh.statusCode).toBe(200);
-      const response = await request(app).get('/api/record/person/person:8016283357279641');
+      const response = await request(app).get(
+        '/api/record/person/person:8016283357279641?presentationRecordLinkId=divaPersonOutputPLink'
+      );
       expect(response.statusCode).toBe(200);
       expect(response.body).toStrictEqual({
-        id: 'person:8016283357279641',
-        recordType: 'person',
-        validationType: 'person',
         createdAt: '2021-02-12T10:55:49.032650Z',
-        updated: [],
-        userRights: ['read', 'read_incoming_links'],
         data: {
           person: {
-            authorisedName: {
-              familyName: [{ value: 'Efternamn' }],
-              givenName: [{ value: 'Förnamn' }]
-            },
-            academicTitle: [{ value: 'Akademisk titel' }],
+            Libris_ID: [
+              {
+                value: '123456'
+              }
+            ],
+            ORCID_ID: [
+              {
+                value: '1111-2222-3333-4444'
+              }
+            ],
+            VIAF_ID: [
+              {
+                value: '123456'
+              }
+            ],
+            academicTitle: [
+              {
+                value: 'Akademisk titel'
+              }
+            ],
             alternativeName: [
               {
-                familyName: [{ value: 'Alternativt efternamn' }],
-                givenName: [{ value: 'Alternativt förnamn' }]
+                familyName: [
+                  {
+                    value: 'Alternativt efternamn'
+                  }
+                ],
+                givenName: [
+                  {
+                    value: 'Alternativt förnamn'
+                  }
+                ]
               }
             ],
-            externalURL: [{ linkTitle: { value: 'Länktext' }, URL: { value: 'http://xx.se' } }],
-            otherAffiliation: [
-              {
-                affiliation: { value: 'Annan organisation' },
-                affiliationFromYear: [{ value: '1980' }],
-                affiliationUntilYear: [{ value: '1990' }]
-              }
-            ],
-            ORCID_ID: [{ value: '1111-2222-3333-4444' }],
-            VIAF_ID: [{ value: '123456' }],
-            Libris_ID: [{ value: '123456' }],
+            authorisedName: {
+              familyName: [
+                {
+                  value: 'Efternamn'
+                }
+              ],
+              givenName: [
+                {
+                  value: 'Förnamn'
+                }
+              ]
+            },
             biographyEnglish: [
-              { biography: { value: 'Biografi på engelska.' }, language: { value: 'en' } }
+              {
+                biography: {
+                  value: 'Biografi på engelska.'
+                },
+                language: {
+                  value: 'en'
+                }
+              }
             ],
             biographySwedish: [
-              { biography: { value: 'Biografi på svenska.' }, language: { value: 'sv' } }
+              {
+                biography: {
+                  value: 'Biografi på svenska.'
+                },
+                language: {
+                  value: 'sv'
+                }
+              }
+            ],
+            externalURL: [
+              {
+                URL: {
+                  value: 'http://xx.se'
+                },
+                linkTitle: {
+                  value: 'Länktext'
+                }
+              }
+            ],
+            otherAffiliation: [
+              {
+                affiliation: {
+                  value: 'Annan organisation'
+                },
+                affiliationFromYear: [
+                  {
+                    value: '1980'
+                  }
+                ],
+                affiliationUntilYear: [
+                  {
+                    value: '1990'
+                  }
+                ]
+              }
             ]
+          }
+        },
+        id: 'person:8016283357279641',
+        recordType: 'person',
+        updated: [],
+        userRights: ['read', 'read_incoming_links'],
+        validationType: 'person',
+        presentation: {
+          form: {
+            childStyle: [],
+            components: [
+              {
+                childStyle: ['compactChildStyle'],
+                components: [
+                  {
+                    childStyle: [],
+                    gridColSpan: 12,
+                    inputType: 'input',
+                    label: 'lastNameTextVarText',
+                    mode: 'output',
+                    name: 'familyName',
+                    repeat: {
+                      minNumberOfRepeatingToShow: 1,
+                      repeatMax: 1,
+                      repeatMin: 0
+                    },
+                    showLabel: true,
+                    tooltip: {
+                      body: 'lastNameTextVarDefText',
+                      title: 'lastNameTextVarText'
+                    },
+                    type: 'textVariable',
+                    validation: {
+                      pattern: '.+',
+                      type: 'regex'
+                    }
+                  },
+                  {
+                    childStyle: [],
+                    components: [
+                      {
+                        childStyle: [],
+                        gridColSpan: 12,
+                        name: 'commaText',
+                        type: 'text'
+                      },
+                      {
+                        childStyle: [],
+                        gridColSpan: 12,
+                        name: 'spaceText',
+                        type: 'text'
+                      },
+                      {
+                        childStyle: [],
+                        gridColSpan: 12,
+                        inputType: 'input',
+                        label: 'firstNameTextVarText',
+                        mode: 'output',
+                        name: 'givenName',
+                        repeat: {
+                          minNumberOfRepeatingToShow: 1,
+                          repeatMax: 1,
+                          repeatMin: 0
+                        },
+                        showLabel: true,
+                        tooltip: {
+                          body: 'firstNameTextVarDefText',
+                          title: 'firstNameTextVarText'
+                        },
+                        type: 'textVariable',
+                        validation: {
+                          pattern: '.+',
+                          type: 'regex'
+                        }
+                      }
+                    ],
+                    containerType: 'surrounding',
+                    gridColSpan: 12,
+                    mode: 'output',
+                    name: 'personNameLinkSContainer',
+                    presentationStyle: 'inline',
+                    type: 'container'
+                  }
+                ],
+                gridColSpan: 12,
+                label: 'personNameGroupText',
+                mode: 'output',
+                name: 'authorisedName',
+                repeat: {
+                  minNumberOfRepeatingToShow: 1,
+                  repeatMax: 1,
+                  repeatMin: 1
+                },
+                showLabel: false,
+                tooltip: {
+                  body: 'personNameGroupDefText',
+                  title: 'personNameGroupText'
+                },
+                type: 'group'
+              }
+            ],
+            gridColSpan: 12,
+            label: 'personGroupText',
+            mode: 'output',
+            name: 'person',
+            repeat: {
+              repeatMax: 1,
+              repeatMin: 1
+            },
+            showLabel: false,
+            tooltip: {
+              body: 'personGroupDefText',
+              title: 'personGroupText'
+            },
+            type: 'group'
           }
         }
       });
