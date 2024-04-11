ddfac66c0ef5033ef57bc744823ed8c369cfeb9a
egiiiiil <sixteenth.bird@gmail.com>
2024-04-11 08:57:29 +0200

    Updated vitest.config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: ['**/*.{test,spec}.{ts,mts,cts,tsx}'],
+    exclude: [
+      '**/node_modules/**',
+      '**/target/**',
+      '**/*.{test,spec}.{js,mjs,cjs,jsx}'
+    ],
     setupFiles: 'setupTest.js',
   },
 });
