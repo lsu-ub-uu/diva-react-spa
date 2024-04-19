7f5480d59467b49f5da8d3962dea476a5a0b0897
egiiiiil <sixteenth.bird@gmail.com>
2024-04-19 17:51:54 +0200

    Refactored form utils

---
+++ src/main/webapp/components/FormGenerator/utils/helper.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2024 Uppsala University Library
+ *
+ * This file is part of DiVA Client.
+ *
+ *     DiVA Client is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     DiVA Client is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with DiVA Client.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { FormComponent } from '../types';
+
+export const countStringCharOccurrences = (
+  inputString: string,
+  targetChar: string,
+) => {
+  return inputString.split('').filter((char) => char === targetChar).length;
+};
+
+export const isFirstLevel = (pathName: string) => {
+  return countStringCharOccurrences(pathName, '.') === 1;
+};
+
+export const isComponentVariable = (component: FormComponent) =>
+  [
+    'numberVariable',
+    'textVariable',
+    'collectionVariable',
+    'recordLink',
+  ].includes(component.type);
+
+export const isComponentGroup = (component: FormComponent) =>
+  component.type === 'group';
+
+export const isComponentContainer = (component: FormComponent) =>
+  component.type === 'container';
+
+export const isComponentSurroundingContainer = (component: FormComponent) =>
+  isComponentContainer(component) && component.containerType === 'surrounding';
+
+export const isComponentRepeatingContainer = (
+  component: FormComponent | undefined,
+) =>
+  component !== undefined &&
+  isComponentContainer(component) &&
+  component.containerType === 'repeating';
+
+export const isComponentValidForDataCarrying = (component: FormComponent) =>
+  isComponentVariable(component) ||
+  isComponentGroup(component) ||
+  isComponentContainer(component); // a container can have children that are data carriers
+
+export const isComponentRepeating = (component: FormComponent) => {
+  const rMax = component.repeat?.repeatMax ?? 1;
+  const rMin = component.repeat?.repeatMin ?? 1;
+  return !(rMax === 1 && rMin === 1);
+};
+
+export const isComponentRequired = (component: FormComponent) => {
+  const rMin = component.repeat?.repeatMin ?? 1;
+  return rMin > 0;
+};
+
+export const isComponentSingularAndOptional = (component: FormComponent) => {
+  const rMax = component.repeat?.repeatMax ?? 1;
+  const rMin = component.repeat?.repeatMin ?? 1;
+  return rMax === 1 && rMin === 0;
+};
