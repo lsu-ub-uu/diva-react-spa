e6f4b67222918c560221f1d4fd5c8b3f8d877fb8
Leo Danielsson <leo.danielsson@syson.se>
2024-10-21 11:08:23 +0200

    remove date picker

--- src/main/webapp/components/Controlled/index.ts
+++ src/main/webapp/components/Controlled/index.ts
@@ -20,7 +20,5 @@ export { ControlledTextField } from './TextField/ControlledTextField';
 export { ControlledSelectField } from './SelectField/ControlledSelectField';
 export { ControlledMultiCheckboxField } from './MultiCheckboxField/ControlledMultiCheckboxField';
 export { ControlledRadioButtons } from './RadioButtons/ControlledRadioButtons';
-export { ControlledDatePicker } from './DatePicker/ControlledDatePicker';
-export { ControlledDateTimePicker } from './DateTimePicker/ControlledDateTimePicker';
 export { ControlledEditor } from './Editor/ControlledEditor';
 export { ControlledLinkedRecord } from './LinkedRecord/ControlledLinkedRecord';
