32236f4ac8609bd145a2d10a6903dfff18bd1c18
Leo Danielsson <leo.danielsson@syson.se>
2024-10-21 11:12:56 +0200

    Remove RadioButton

--- src/main/webapp/components/Controlled/index.ts
+++ src/main/webapp/components/Controlled/index.ts
@@ -19,5 +19,4 @@
 export { ControlledTextField } from './TextField/ControlledTextField';
 export { ControlledSelectField } from './SelectField/ControlledSelectField';
 export { ControlledMultiCheckboxField } from './MultiCheckboxField/ControlledMultiCheckboxField';
-export { ControlledRadioButtons } from './RadioButtons/ControlledRadioButtons';
 export { ControlledLinkedRecord } from './LinkedRecord/ControlledLinkedRecord';
