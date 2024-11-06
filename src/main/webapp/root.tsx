81e704455183e2f02b6fbac1f0681c0cce42f411
Leo Danielsson <leo.danielsson@ub.uu.se>
2024-11-06 10:59:39 +0100

    remove emotion cache code from root route

--- src/main/webapp/root.tsx
+++ src/main/webapp/root.tsx
@@ -26,19 +26,11 @@ import {
 import {
   ActionFunctionArgs,
   json,
-  LinksFunction,
   LoaderFunctionArgs,
   redirect,
 } from '@remix-run/node';
-import { unstable_useEnhancedEffect as useEnhancedEffect } from '@mui/utils';
-import { withEmotionCache } from '@emotion/react';
-import { ReactNode, Suspense, useContext, useEffect } from 'react';
-import ClientStyleContext from '@/ClientStyleContext';
-import {
-  BackdropProvider,
-  Layout as RootLayout,
-  SnackbarProvider,
-} from '@/components';
+import { ReactNode, Suspense, useEffect } from 'react';
+import { BackdropProvider, Layout, SnackbarProvider } from '@/components';
 import store from '@/app/store';
 import { CssBaseline } from '@mui/material';
 import { divaTheme } from '@/themes/diva';
@@ -52,11 +44,8 @@ import i18nServer from '@/app/i18n.server';
 import { useChangeLanguage } from 'remix-i18next/react';
 import { i18nCookie } from '@/app/i18nCookie';
 
-export const links: LinksFunction = () => [];
-
 interface DocumentProps {
   children: ReactNode;
-  title?: string;
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -73,7 +62,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
   const language = formData.get('language');
-  console.log({ request });
   if (typeof language === 'string') {
     return redirect('/', {
       headers: {
@@ -83,71 +71,47 @@ export async function action({ request }: ActionFunctionArgs) {
   }
 }
 
-const Document = withEmotionCache(
-  ({ children, title }: DocumentProps, emotionCache) => {
-    const { locale } = useLoaderData<typeof loader>();
+const Document = ({ children }: DocumentProps) => {
+  const { locale } = useLoaderData<typeof loader>();
+  const { MODE } = import.meta.env;
+  useChangeLanguage(locale);
 
-    const clientStyleData = useContext(ClientStyleContext);
-
-    // Only executed on client
-    useEnhancedEffect(() => {
-      // re-link sheet container
-      emotionCache.sheet.container = document.head;
-      // re-inject tags
-      const tags = emotionCache.sheet.tags;
-      emotionCache.sheet.flush();
-      tags.forEach((tag) => {
-        (emotionCache.sheet as any)._insertTag(tag);
-      });
-      // reset cache to reapply global styles
-      clientStyleData.reset();
-    }, []);
-
-    const { MODE } = import.meta.env;
-
-    return (
-      <html lang={locale}>
-        <head>
-          <meta charSet='utf-8' />
-          <meta
-            name='viewport'
-            content='width=device-width,initial-scale=1'
-          />
-          <meta
-            name='theme-color'
-            content={divaTheme.palette.primary.main}
-          />
-          <link
-            rel='icon'
-            type='image/svg+xml'
-            href={MODE === 'development' ? dev_favicon : favicon}
-          />
-          <title>{title ?? ''}</title>
-          <Meta />
-          <Links />
-          <meta
-            name='emotion-insertion-point'
-            content='emotion-insertion-point'
-          />
-        </head>
-        <body>
-          {children}
-          <ScrollRestoration />
-          <Scripts />
-        </body>
-      </html>
-    );
-  },
-);
-
-export function Layout({ children }: { children: ReactNode }) {
-  return <Document>{children}</Document>;
-}
+  return (
+    <html lang={locale}>
+      <head>
+        <meta charSet='utf-8' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1'
+        />
+        <meta
+          name='theme-color'
+          content={divaTheme.palette.primary.main}
+        />
+        <link
+          rel='icon'
+          type='image/svg+xml'
+          href={MODE === 'development' ? dev_favicon : favicon}
+        />
+        <title>DiVA</title>
+        <Meta />
+        <Links />
+        <meta
+          name='emotion-insertion-point'
+          content='emotion-insertion-point'
+        />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+};
 
 export default function App() {
-  const { auth, locale } = useLoaderData<typeof loader>();
-
-  useChangeLanguage(locale);
+  const { auth } = useLoaderData<typeof loader>();
 
   useEffect(() => {
     axios.defaults.headers.common = {
@@ -156,21 +120,21 @@ export default function App() {
   }, [auth]);
 
   return (
-    <>
-      <CssBaseline />
-      <BackdropProvider>
-        <StateProvider store={store}>
-          <SnackbarProvider maxSnack={5}>
-            <ErrorBoundary
-              fallback={<h1>Something went wrong. Try again later</h1>}
-            >
-              <Suspense fallback={<h3>Waiting for DiVA 3 GUI to load...</h3>}>
-                <RootLayout />
-              </Suspense>
-            </ErrorBoundary>
-          </SnackbarProvider>
-        </StateProvider>
-      </BackdropProvider>
-    </>
+    <Suspense>
+      <Document>
+        <CssBaseline />
+        <BackdropProvider>
+          <StateProvider store={store}>
+            <SnackbarProvider maxSnack={5}>
+              <ErrorBoundary
+                fallback={<h1>Something went wrong. Try again later</h1>}
+              >
+                <Layout />
+              </ErrorBoundary>
+            </SnackbarProvider>
+          </StateProvider>
+        </BackdropProvider>
+      </Document>
+    </Suspense>
   );
 }
