9bc55cb82cd18e78c04014f026165c3e2c9e978a
Egil <sixteenth.bird@gmail.com>
2024-11-20 11:30:37 +0100

    CORA-2690 Added notistack to noExternal for vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig(({ mode }) => {
       optimizeDeps: {
         include: ['lodash'],
       },
-      noExternal: ['@mui/icons-material', 'lodash'],
+      noExternal: ['@mui/icons-material', 'lodash', 'notistack'],
     },
     resolve:
       mode === 'production'
