0915820dffbd752e951d9d155c3fa8109a935a79
Leo Danielsson <leo.danielsson@ub.uu.se>
2024-12-06 14:08:13 +0100

    remove unused files

--- app/mui/MuiProvider.tsx
+++ app/mui/MuiProvider.tsx
@@ -3,7 +3,7 @@ import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material';
 
 import createCache from '@emotion/cache';
-import { divaTheme } from '@/theme';
+import { divaTheme } from '@/mui/theme';
 import { ReactNode } from 'react';
 
 function createEmotionCache() {
